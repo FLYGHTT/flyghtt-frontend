437c56dd5f71c456eff10d98920f7b1ac7d17820
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2024-09-25 02:36:43 +0100

    update editor sidebar

--- flight/components/CreateTool/CTSidebar.tsx
+++ flight/components/CreateTool/CTSidebar.tsx
@@ -1,18 +1,14 @@
+"use client";
 import React from "react";
 import search from "@/assets/icons/search.svg";
 import Image from "next/image";
-import { FaChevronDown } from "react-icons/fa";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import CommunityTools from "./CommunityTools";
+import YourTools from "./YourTools";
 
 const CreateToolSidebar = () => {
+  
   return (
-    <div className="col-span-2 p-4 border-r-gray-600 h-full border">
+    <div className="col-span-2 overflow-y-auto max-h-[calc(100vh-60px)] p-2.5 border-r-gray-600 h-full border">
       <h3 className="text-center text-sm font-medium">Start from a template</h3>
       <div className="flex items-center gap-2 rounded-xl border border-lightGray mt-3  pl-2 w-full">
         <Image src={search} alt="search" width={20} height={20} />
@@ -22,30 +18,9 @@ const CreateToolSidebar = () => {
           placeholder="Search..."
         />
       </div>
-      {/* <Accordion type="multiple" >
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Community</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion> */}
-      <p className="text-xs text-gray-700 mt-3 mx-auto text-center">
-        Community
-      </p>
-      <Tabs
-        defaultValue="top"
-        className="w-full flex items-center justify-center flex-col mt-2"
-      >
-        <TabsList className="w-full">
-          <TabsTrigger value="top">Top</TabsTrigger>
-          <TabsTrigger value="latest">Latest</TabsTrigger>
-        </TabsList>
-        <TabsContent value="top">
-          Make changes to your account here.
-        </TabsContent>
-        <TabsContent value="latest">Change your password here.</TabsContent>
-      </Tabs>
+
+      <CommunityTools />
+      <YourTools />
     </div>
   );
 };
